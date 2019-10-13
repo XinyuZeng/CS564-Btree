89d6c0bd20891fedd40c4f5919c9435c4f328464
xyzeng <xzeng56@wisc.edu>
2019-10-12 19:02:49 -0500

    init

---
+++ src/btree.cpp
@@ -0,0 +1,104 @@
+/**
+ * @author See Contributors.txt for code contributors and overview of BadgerDB.
+ *
+ * @section LICENSE
+ * Copyright (c) 2012 Database Group, Computer Sciences Department, University of Wisconsin-Madison.
+ */
+
+#include "btree.h"
+#include "filescan.h"
+#include "exceptions/bad_index_info_exception.h"
+#include "exceptions/bad_opcodes_exception.h"
+#include "exceptions/bad_scanrange_exception.h"
+#include "exceptions/no_such_key_found_exception.h"
+#include "exceptions/scan_not_initialized_exception.h"
+#include "exceptions/index_scan_completed_exception.h"
+#include "exceptions/file_not_found_exception.h"
+#include "exceptions/end_of_file_exception.h"
+
+
+//#define DEBUG
+
+namespace badgerdb
+{
+
+// -----------------------------------------------------------------------------
+// BTreeIndex::BTreeIndex -- Constructor
+// -----------------------------------------------------------------------------
+
+BTreeIndex::BTreeIndex(const std::string & relationName,
+		std::string & outIndexName,
+		BufMgr *bufMgrIn,
+		const int attrByteOffset,
+		const Datatype attrType)
+{
+    // create blobfile, fill in metainfo, etc
+
+    //fill the newly created Blob File using filescan
+    FileScan fileScan(relationName, bufMgr);
+    RecordId rid;
+    try
+    {
+        while(1)
+        {
+            fileScan.scanNext(rid);
+            std::string record = fileScan.getRecord();
+            insertEntry(record.c_str() + attrByteOffset, rid);
+        }
+    }
+    catch(EndOfFileException e)
+    {
+        // save Btee index file to disk
+        bufMgr->flushFile(file);
+    }
+}
+
+
+// -----------------------------------------------------------------------------
+// BTreeIndex::~BTreeIndex -- destructor
+// -----------------------------------------------------------------------------
+
+BTreeIndex::~BTreeIndex()
+{
+}
+
+// -----------------------------------------------------------------------------
+// BTreeIndex::insertEntry
+// -----------------------------------------------------------------------------
+
+const void BTreeIndex::insertEntry(const void *key, const RecordId rid) 
+{
+
+}
+
+// -----------------------------------------------------------------------------
+// BTreeIndex::startScan
+// -----------------------------------------------------------------------------
+
+const void BTreeIndex::startScan(const void* lowValParm,
+				   const Operator lowOpParm,
+				   const void* highValParm,
+				   const Operator highOpParm)
+{
+
+}
+
+// -----------------------------------------------------------------------------
+// BTreeIndex::scanNext
+// -----------------------------------------------------------------------------
+
+const void BTreeIndex::scanNext(RecordId& outRid) 
+{
+
+}
+
+// -----------------------------------------------------------------------------
+// BTreeIndex::endScan
+// -----------------------------------------------------------------------------
+//
+const void BTreeIndex::endScan() 
+{
+
+}
+
+}
